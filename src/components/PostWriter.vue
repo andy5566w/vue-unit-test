67e84a4653d29298c1788c20763c550ac23b4cd6
andy.tsai <andy.tsai@deephow.com>
2021-11-13 14:48:43 +0800

    feat: add debounce

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -19,7 +19,20 @@ import { defineComponent, ref, onMounted, watch } from 'vue'
 import { Post } from '@/mocks'
 import { parse } from 'marked'
 import highlight from 'highlight.js'
-
+import debounce from 'lodash/debounce'
+// eslint-disable-next-line @typescript-eslint/ban-types
+// const debounce = (fn: Function, second: number) => {
+//   let timer: any = null
+//   const fb = function (arg: string) {
+//     // eslint-disable-next-line @typescript-eslint/no-this-alias
+//     // const context: typeof fb = this
+//     clearTimeout(timer)
+//     timer = setTimeout(() => {
+//       fn.call(this, arg)
+//     }, second)
+//   }
+//   return fb
+// }
 export default defineComponent({
   name: 'PostWriter',
   props: {
@@ -34,16 +47,18 @@ export default defineComponent({
     const contenteditable = ref<null | HTMLElement>(null)
     const html = ref('')
 
+    const parseHtml = (str: string) => {
+      html.value = parse(str, {
+        gfm: true,
+        breaks: true,
+        highlight: (code: string) => highlight.highlightAuto(code).value,
+      })
+    }
     watch(
       content,
-      (newContent) => {
-        html.value = parse(newContent, {
-          gfm: true,
-          breaks: true,
-          highlight: (code: string) => highlight.highlightAuto(code).value,
-        })
-        console.log(html.value)
-      },
+      debounce((newContent: string) => {
+        parseHtml(newContent)
+      }, 500),
       { immediate: true }
     )
     onMounted(() => {
